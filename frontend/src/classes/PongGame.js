7b96bac0baf4c469536d482efa4ec5f197fb7cdc
nprosk <proskauer.n@northeastern.edu>
2024-03-19 16:46:57 -0400

    more pong stuff, drawing ball, moving paddles

--- frontend/src/classes/PongGame.js
+++ frontend/src/classes/PongGame.js
@@ -5,10 +5,6 @@ const config = {
     parent: "phaser-example",
     width: 800,
     height: 640,
-    scale: {
-        mode: Phaser.Scale.RESIZE,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
     scene: {
         preload: preload,
         create: create,
@@ -23,17 +19,65 @@ const config = {
     }
 };
 
+
 let ball;
+let player1;
+let player2;
+let gameInitialized = false;
 
 function preload() {
-    this.load.image("ball", "../../public/assets/ball.png");
-    this.load.image("paddle", "../../public/assets/paddle.png");
+    this.load.image("ball", "/assets/pong_images/ball.png");
+    this.load.image("paddle", "/assets/pong_images/paddle.png");
 }
 
 function create() {
+    ball = this.physics.add.sprite(
+        this.physics.world.bounds.width / 2,
+        this.physics.world.bounds.height / 2,
+        "ball"
+    );
+    ball.setCollideWorldBounds(true);
+    ball.setBounce(1, 1);
+
+    player1 = this.physics.add.sprite(
+        this.physics.world.bounds.width - (ball.body.width / 2 + 1),
+        this.physics.world.bounds.height / 2,
+        "paddle"
+    );
+    player1.setCollideWorldBounds(true);
+
+    player2 = this.physics.add.sprite(
+        (ball.body.width / 2 + 1),
+        this.physics.world.bounds.height / 2,
+        "paddle"
+    );
+    player2.setCollideWorldBounds(true);
+
 }
 
 function update() {
+    if (!gameInitialized) {
+        initializeGame();
+    }
+    if (this.input.keyboard.checkDown(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W), 500)) {
+        player1.setVelocity(0, -300);
+    } else if (this.input.keyboard.checkDown(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S), 500)) {
+        player1.setVelocity(0, 300);
+    } else {
+        player1.setVelocity(0, 0);
+    }
+    if (this.input.keyboard.checkDown(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP), 500)) {
+        player2.setVelocity(0, -300);
+    } else if (this.input.keyboard.checkDown(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN), 500)) {
+        player2.setVelocity(0, 300);
+    } else {
+        player2.setVelocity(0, 0);
+    }
+}
+
+function initializeGame() {
+    ball.setVelocity(100, 100);
+    gameInitialized = true;
 }
 
 export function PongGame() {
