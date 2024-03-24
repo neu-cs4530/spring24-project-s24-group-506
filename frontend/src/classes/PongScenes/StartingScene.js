41583422f7c0e2d79e9fea99463ebc208913dea6
nprosk <proskauer.n@northeastern.edu>
2024-03-24 18:27:58 -0400

    Working starting scene and game to 5 locally

---
+++ frontend/src/classes/PongScenes/StartingScene.js
@@ -0,0 +1,17 @@
+export class StartingScene extends Phaser.Scene {
+  constructor() {
+    super('StartingScene');
+  }
+
+  create() {
+    this.add.text(
+      this.physics.world.bounds.width / 2,
+      this.physics.world.bounds.height / 2,
+      'Press SPACE to start',
+      { fontSize: '32px', fill: '#fff' },
+    );
+    this.input.keyboard.on('keydown-SPACE', () => {
+      this.scene.start('PlayingScene');
+    });
+  }
+}
