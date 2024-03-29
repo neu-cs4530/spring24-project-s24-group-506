4cd5c0f20e1ff05da862459029121737af653dff
rahildacoder <rahilbadkul@gmail.com>
2024-03-29 17:57:15 -0400

    adding comment for function for purchase of an item

--- townService/src/town/TicketBoof.ts
+++ townService/src/town/TicketBoof.ts
@@ -6,4 +6,13 @@ export default class TicketBooth {
   public constructor(itemsOnSale: [BoothItem, number, number][]) {
     this._itemsOnSale = itemsOnSale;
   }
+
+  /**
+   * Handles a purchase from a player.
+   *
+   * Updates the player's tokenCount and the third value for itemsOnSale.
+   *
+   * @param player The player making the purchase
+   * @param item The item being purchased
+   */
 }
