4db860f66177e1683f6c697d359d4a35e05f9af7
akash3116 <alaparthiakash@gmail.com>
2024-04-09 01:25:52 -0400

    removed unused import

--- townService/src/town/games/PongGameArea.test.ts
+++ townService/src/town/games/PongGameArea.test.ts
@@ -4,7 +4,6 @@ import { createPlayerForTesting } from '../../TestUtils';
 import {
   GAME_ID_MISSMATCH_MESSAGE,
   GAME_NOT_IN_PROGRESS_MESSAGE,
-  INVALID_COMMAND_MESSAGE,
 } from '../../lib/InvalidParametersError';
 import Player from '../../lib/Player';
 import PongGameArea from './PongGameArea';
